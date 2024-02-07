e1cf19a89c1cf74caf076eca4f182f1f21ca7991
ysubbagh <ydns10@gmail.com>
2024-02-06 21:42:41 -0800

    local python broken

--- backup.py
+++ backup.py
@@ -5,12 +5,46 @@
 
 import sys
 import boto3
+import os
 
 
-if(len(sys.argv) != 2):
-    sys.exit("Error: Improper arguemnts passed.")
+def main():
+    args = sys.argv
+    #check arguemtns length is correct
+    if(len(args) != 2):
+        sys.exit("Error: Improper arguemnts passed.")
 
-#connect to s3
-session = boto3.Session(profile_name='dev')
-client = boto3.client('s3')
+    #parse string from arguemnts
+    args = args.split(' ')
+    local_path = args[0]
+    args = args[1].split('::')
+    bucket_path = args[0]
+    cloud_path = [1]
+
+    #check for empty arguemnts
+    if(local_path == "" or bucket_path == "" or cloud_path == ""):  
+        sys.exit("Error: Improper arguemnts passed.")
+
+    #print paths
+    print(f"local path: {local_path}")
+    print(f"bucket: {bucket_path}")
+    print(f"cloud path: {cloud_path}")
+
+
+
+#recursivly backup to aws
+def backup(local_path, bucket_name, cloud_path):
+    #connect to s3
+    client = boto3.client('s3')
+
+    #check to see if bucket exists, create if not
+    try:
+        resp = client.head_bucket(Bucket = bucket_name)
+    except client.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == '404': #if bucket doesnt already exist, create it
+            try:
+                create = client.create_bucket(Bucket = bucket_name)
+            except Exception as e:
+                sys.exit("Error: Couldn't create bucket.")
+    
 
--- restore.py
+++ restore.py
@@ -1,7 +1,35 @@
 #
-# restore -- p3. css436
+# backup -- p3. css436
 # Author: Yasmine Subbagh
 #
 
 import sys
+import boto3
+import os
+
+
+def main():
+    args = sys.argv
+    #check arguemtns length is correct
+    if(len(args) != 2):
+        sys.exit("Error: Improper arguemnts passed.")
+
+    #parse string from arguemnts
+    args = args.split(' ')
+    local_path = args[1]
+    args = args[0].split('::')
+    bucket_path = args[0]
+    cloud_path = [1]
+
+    #check for empty arguemnts
+    if(local_path == "" or bucket_path == "" or cloud_path == ""):  
+        sys.exit("Error: Improper arguemnts passed.")
+
+
+
+#recursivly backup to aws
+def restore():
+    #connect to s3
+    client = boto3.client('s3')
+
 
